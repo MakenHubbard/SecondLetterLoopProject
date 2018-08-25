631ca5827dbbf0b4fa165049b90aa2dd0e19bcfd
Maken Hubbard <makhub26@outlook.com>
2018-08-25 10:32:46 -0500

    adding code for the the second letter loop

--- SecondLetterLoopProject/Program.cs
+++ SecondLetterLoopProject/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Linq;
 
 namespace SecondLetterLoopProject
 {
     class Program
     {
         static void Main(string[] args)
+        // INPUT = RqaEzty
+        // DESIRED OUTPUT = R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy
         {
-            Console.WriteLine("Hello World!");
+            var letters = "RqaEzty";
+
+            /* The line below is taking the string and using the string.Join() method the join the contents of the string with a - in between and 
+               then using the .Select() method to take the in the char and the index of that to add the equivalent number of the same character to it and capitlizing the first and lowering the case on the rest */
+
+            var output = string.Join("-", letters.Select((c, i) => Char.ToUpper(c) + new string(Char.ToLower(c), i)));
+            Console.WriteLine(output);
+            Console.ReadLine();
+
         }
     }
 }
